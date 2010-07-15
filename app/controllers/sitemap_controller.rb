b9dd082b2f5fa6b4bf6a58a3ad513f58a2af9e48
Paul Brower <brower.paul@gmail.com>
2010-07-14 23:20:56 -0400

    blog, ui changes

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,6 +3,7 @@ class SitemapController < ApplicationController
   def sitemap
     @entries = System.find(:all, :order => "updated_at DESC", :limit => 50000)
     @profiles = Profile.find(:all, :order => "updated_at DESC", :limit => 50000) 
+    @posts = Post.find(:all, :order => "updated_at DESC", :limit => 50000) 
     headers["Content-Type"] = "text/xml"
     # set last modified header to the date of the latest entry.
     headers["Last-Modified"] = @entries[0].updated_at.httpdate
